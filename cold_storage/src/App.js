22cbef69b734c13b84bdbb5793e89ef607f00f1f
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2025-01-19 17:07:17 -0500

    log in page done

--- cold_storage/src/App.js
+++ cold_storage/src/App.js
@@ -1,15 +1,35 @@
 import logo from './cold_storage.png';
 import './App.css';
+import Login from './components/Login';
+import { Routes, Route, useNavigate, Navigate } from 'react-router-dom';
 
-function App() {
+const HomePage = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>
           Cold Storage
         </h1>
-      </header>
+        <div>
+          <button onClick={() => navigate("/login")}>Go to Login</button>
+          <button onClick={() => navigate("/signup")}>Go to Signup</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
     </div>
   );
 }
